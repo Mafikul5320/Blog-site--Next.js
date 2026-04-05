a88664880daa104694f7e8627e23af99785855f4
Mafikul Islam <mdmafikulislam5320@gmail.com>
2026-04-05 23:45:18 +0600

    Dashboard route create

--- src/app/(DashvoardLayout)/dashboard/page.tsx
+++ src/app/(DashvoardLayout)/dashboard/page.tsx
@@ -1,20 +1,5 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-
-export default function Page() {
+export default function Dashboard() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-
-      <SidebarInset>
-        <header className="flex h-16 items-center border-b px-4">
-          <SidebarTrigger />
-        </header>
-
-        <div className="p-4">
-          Side Ber Content
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+    <div>Dashboard page</div>
   )
 }
--- src/app/(DashvoardLayout)/layout.tsx
+++ src/app/(DashvoardLayout)/layout.tsx
@@ -1,5 +1,20 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
 export default function layout({ children }: { children: React.ReactNode }) {
     return (
-        <div>{children}</div>
+        <SidebarProvider>
+            <AppSidebar />
+
+            <SidebarInset>
+                <header className="flex h-16 items-center border-b px-4">
+                    <SidebarTrigger />
+                </header>
+
+                <div className="p-4">
+                    {children}
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
     )
 }
